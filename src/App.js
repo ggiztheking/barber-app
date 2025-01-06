4e15736645aa86a7a857d1f02e7ea5762b96add0
ggiztheking <artemindstudio@gmail.com>
2025-01-05 20:25:50 -0600

    corregido archivo App.js y rutas

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Barber App</h1>
+      <p>¡Bienvenido a la Barbería!</p>
+    </div>
+  );
+}
+
+export default App;
